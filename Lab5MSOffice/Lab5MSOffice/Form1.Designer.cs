f34403982f41274eb13021689394c359e73abdfb
Ekaterina <klifanteva@yandex.ru>
2020-12-07 00:25:05 +0800

    lab5 to lab7 repository commit

---
+++ Lab5MSOffice/Lab5MSOffice/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Lab5MSOffice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.launchMSWordButton = new System.Windows.Forms.Button();
+            this.launchMSExcelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(23, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 66);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MS Word\r\nОткрытие существующего документа D:/Lab5.docx. Добавление текстовой стро" +
+    "ки. Стиль вводимого теста Полужирный.";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(298, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 46);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MS Excel\r\nОбъединение содержимого 2-х документов.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Текст:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(75, 133);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // launchMSWordButton
+            // 
+            this.launchMSWordButton.Location = new System.Drawing.Point(75, 181);
+            this.launchMSWordButton.Name = "launchMSWordButton";
+            this.launchMSWordButton.Size = new System.Drawing.Size(75, 23);
+            this.launchMSWordButton.TabIndex = 9;
+            this.launchMSWordButton.Text = "Выполнить";
+            this.launchMSWordButton.UseVisualStyleBackColor = true;
+            this.launchMSWordButton.Click += new System.EventHandler(this.launchMSWordButton_Click);
+            // 
+            // launchMSExcelButton
+            // 
+            this.launchMSExcelButton.Location = new System.Drawing.Point(345, 112);
+            this.launchMSExcelButton.Name = "launchMSExcelButton";
+            this.launchMSExcelButton.Size = new System.Drawing.Size(75, 23);
+            this.launchMSExcelButton.TabIndex = 10;
+            this.launchMSExcelButton.Text = "Выполнить";
+            this.launchMSExcelButton.UseVisualStyleBackColor = true;
+            this.launchMSExcelButton.Click += new System.EventHandler(this.launchMSExcelButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 289);
+            this.Controls.Add(this.launchMSExcelButton);
+            this.Controls.Add(this.launchMSWordButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button launchMSWordButton;
+        private System.Windows.Forms.Button launchMSExcelButton;
+    }
+}
+
---
+++ Lab5MSOffice/Lab5MSOffice/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Lab5MSOffice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void launchMSWordButton_Click(object sender, EventArgs e)
+        {
+            Word.Application appWord = new Word.Application();
+            Word.Document docWord = null;
+            string source = @"D:\Lab5.docx";
+            docWord = appWord.Documents.Open(source);
+            docWord.Activate();
+            appWord.Visible = true;
+            /*object missing = Type.Missing;
+            Word.Document doc = app.Documents.Add(ref missing, ref missing, ref missing, ref missing);*/
+            Word.Paragraph para = docWord.Paragraphs.Add();
+            docWord.Paragraphs[1].Range.Bold = 2;
+            docWord.Paragraphs[1].Range.Text = this.textBox2.Text;
+        }
+
+        private void launchMSExcelButton_Click(object sender, EventArgs e)
+        {
+            
+            Excel.Application appExcel = new Excel.Application();
+            Excel.Workbook workbook1;//книга откуда копировать
+            Excel.Workbook workbook2;//книга куда копировать
+            Excel.Worksheet workSheet1;//лист который копировать
+            string source1 = @"D:\Lab5_1.xlsx";
+            workbook1 = appExcel.Workbooks.Open(source1);
+            string source2 = @"D:\Lab5_2.xlsx";
+            workbook2 = appExcel.Workbooks.Open(source2);
+            workSheet1 = workbook1.Worksheets["Лист1"];
+            workSheet1.Copy(After: workbook2.Worksheets[workbook2.Worksheets.Count]);
+            MessageBox.Show("Лист '" + workSheet1.Name.ToString() + "' успешно скопирован", "Поиск", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            appExcel.Visible = true;
+            workbook1.Save();
+            workbook1.Close();
+            workbook2.Save();
+            workbook2.Close();
+            appExcel.Quit();
+            
+        }
+    }
+}
